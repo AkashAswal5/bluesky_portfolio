3a8e890d4c289c0da3837c6ca9008f2b7a4193cd
Mohit <bluesky5april2021@gmail.com>
2025-06-14 15:13:39 +0530

    basic error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { TalkComponent } from './talk/talk.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:HomeComponent},
+    {path:"features/:solution", loadComponent:() => FeaturesComponent},
+    {path:"contact", loadComponent:()=> TalkComponent},
+    {path:"**", component:ErrorComponent}
+];
