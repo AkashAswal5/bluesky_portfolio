be3fb5946303eaa5c84c991109e6f6e9506aefca
Mohit <bluesky5april2021@gmail.com>
2025-06-19 16:29:37 +0530

    improved contact us design, improved navbar and added infinitely translating backgound

--- src/app/talk/talk.component.ts
+++ src/app/talk/talk.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './talk.component.css'
 })
 export class TalkComponent {
-
+  times = ["30 min", "45 min", "60 min", "90 min"];
+  about = "Hi, we are a team of developers who are passionate about building great software. We are dedicated to creating high-quality applications that solve real-world problems. Our team has a diverse set of skills and experiences, allowing us to tackle a wide range of challenges. We believe in continuous learning and improvement, and we are always looking for new ways to enhance our skills and knowledge. We are excited to share our journey with you and hope to inspire others to pursue their passions in software development XD"
 }
