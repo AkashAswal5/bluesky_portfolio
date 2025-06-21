3f506abbdb760025ceb022de3c1376813101e1bc
Mohit <bluesky5april2021@gmail.com>
2025-06-21 19:13:44 +0530

    reversed background style, fixed feature page'

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  save_time_text = "Analyzing payloads...";
+  
+  ngOnInit(): void {
+    this.changeText();
+  }
+  changeText(){
+    const save_time_text_list = ["Scanning ports...", "Detecting threats...", "Analyzing vulnerabilities...", "Generating report..."]
+    let i =0
+    setInterval(()=>{
+      this.save_time_text = save_time_text_list[i]
+      i = (i + 1) % save_time_text_list.length
+    }, 3000)
+  }
 }
